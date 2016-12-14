1258209c58d3e19d60cbac2a3cf7d9e24115fecf
ken koontz <kenneth.koontz@gmail.com>
2016-12-14 11:14:47 -0800

    horses

---
+++ sh/rightheight.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+say 'Good Morning Greg!';
+wget -q http://www.theimaginativeconservative.org/wp-content/uploads/2016/06/horse.jpg
+open horse.jpg
+sleep 1
+say "That horse, it's a beautiful one right?"
+say "Oh gosh this is such a rush."
+say "Nothing better than this right here."
+sleep 1
+
+say "I'd say it's JRHNBR."
+say "What do you think?"
+say "Oh gosh so exhilerating"
+
+rm horse.jpg
