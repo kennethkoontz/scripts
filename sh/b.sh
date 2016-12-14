53eff618b515795ee6119589f53e0cfdaca10024
ken koontz <kenneth.koontz@gmail.com>
2016-12-14 11:53:38 -0800

    league

---
+++ sh/b.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+say "Hello. Good morning!"
+sleep 1
+say "Do you feel empty inside?"
+sleep 1
+say "Would you like to get it on in a place where there is nothing but kicking ass in the rift?"
+sleep 1
+say "Well fret not. I've got you're back. Let's league!"
+open /Applications/League\ of\ Legends.app/Contents/LoL/Play\ League\ Of\ Legends.app
+curl -O http://s.lolstatic.com/site/ekko-comic/c16250df6f22e04fcec07cd023bd6ddcf5b5c8f1/img/share/ekko-share-1200x627.jpg
+open ekko-share-1200x627.jpg
+say "Summoners prepare to kick ass!"
+rm ekko*
+
