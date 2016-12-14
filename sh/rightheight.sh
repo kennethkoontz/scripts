a21b5a9461a61b49a146743ee272744d90c8b8a9
ken koontz <kenneth.koontz@gmail.com>
2016-12-14 11:23:10 -0800

    update

--- sh/rightheight.sh
+++ sh/rightheight.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-say 'Good Morning Greg!';
+say 'Good Morning! How do you do?';
+sleep 1
+say "I'd like to show you something";
+sleep 1
+say "Wait for it."
+sleep 1
+say "Wait for it."
+sleep 1
 wget -q http://www.theimaginativeconservative.org/wp-content/uploads/2016/06/horse.jpg
 open horse.jpg
 sleep 1
+say "Ahh, it's there."
 say "That horse, it's a beautiful one right?"
 say "Oh gosh this is such a rush."
 say "Nothing better than this right here."
@@ -10,6 +18,6 @@ sleep 1
 
 say "I'd say it's JRHNBR."
 say "What do you think?"
-say "Oh gosh so exhilerating"
+say "Oh gosh, this is so exhilerating"
 
 rm horse.jpg
